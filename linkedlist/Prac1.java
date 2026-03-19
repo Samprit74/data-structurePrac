7a0a1b9d5591f20cf449005a530dc97e820df686
Samprit Roy <sampritroy99@gmail.com>
2026-03-20 04:53:37 +0530

    Initial commit

---
+++ linkedlist/Prac1.java
@@ -0,0 +1,11 @@
+public class Prac1 {
+
+    private
+
+
+
+
+    public static void main(String[] args) {
+       
+    }
+}
---
+++ linkedlist/SinglyLinkedList.java
@@ -0,0 +1,16 @@
+public class SinglyLinkedList {
+    private Node head;
+
+    private static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+
+
+
+}
